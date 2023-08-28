3b702e06c170e840b94be4bb1b3ff2e1eee1cca9
edwinkim16 <edwinkimathi226@gmail.com>
2023-08-28 12:03:09 +0300

    adding calendar layout and its js functions

--- Scripts/main.js
+++ Scripts/main.js
@@ -21,3 +21,27 @@ function divide(){
     var num2 = parseFloat(document.getElementById("num2").value);
     document.getElementById("result").textContent = num1 / num2;
 }
+
+// Calendar code 
+
+const calendarContainer = document.getElementById("calendar");
+function createCalendar(year,month){
+    const daysInMonth = new Date (year, month + 1,0).getDate();
+    const firstDayOfWeek = new Date(year,month,1).getDay();
+
+    let calendarHTML = "";
+
+    for (let i=0;i<firstDayOfWeek;i++){
+        calendarHTML+= "<div class='day'></div";
+    }
+
+    for (let day=1;day<=daysInMonth;day++){
+        calendarHTML+= "<div class = 'day'> ${day}</div>";
+    }
+
+    calendarContainer.innerHTML = calendarHTML;
+
+}
+
+const currentDate = new Date();
+createCalendar(currentDate.getFullYear(), currentDate.getMonth());
