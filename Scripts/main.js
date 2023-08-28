3bd21c92f408ac2cbef25c63baf4aa27d0cb9626
edwinkim16 <edwinkimathi226@gmail.com>
2023-08-28 10:55:31 +0300

    linking buttons to js functions and debugging the results

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,23 +1,23 @@
 function add(){
-    var num1 = parseFloat(document.getElementById("num1")).valueOf;
-    var num2 = parseFloat(document.getElementsById("num2")).valueOf;
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
     document.getElementById("result").textContent = num1 + num2;
 }
 
 function subtract(){
-    var num1 = parseFloat(document.getElementById("num1")).valueOf;
-    var num2 = parseFloat(document.getElementsById("num2")).valueOf;
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
     document.getElementById("result").textContent = num1 - num2;
 }
 
 function multiply(){
-    var num1 = parseFloat(document.getElementById("num1")).valueOf;
-    var num2 = parseFloat(document.getElementsById("num2")).valueOf;
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
     document.getElementById("result").textContent = num1 * num2;
 }
 
 function divide(){
-    var num1 = parseFloat(document.getElementById("num1")).valueOf;
-    var num2 = parseFloat(document.getElementsById("num2")).valueOf;
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
     document.getElementById("result").textContent = num1 / num2;
 }
